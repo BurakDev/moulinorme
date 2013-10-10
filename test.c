6f9cfad5d3da126935e8640e0155e45a0a3df05c
Adrien Morel <adrien.morel@intersec.com>
2013-10-10 18:50:02 +0200

    converting moulinorme to python. Add a test.c file

---
+++ test.c
@@ -0,0 +1,45 @@
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+#define test
+
+void printf(int a,
+            int b)
+{
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    if(toto)
+    if (toto)
+    {
+    }
+    else if(tata)
+    {
+    }
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    return;
+    return foo;
+    return(yolo);
+    return ;
+    return yolo;
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+    ceci est
+}
+ 
